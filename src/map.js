6e5ea6d3acec27258cbcbcbe5e68cc5f3af69204
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-03-14 02:09:41 -0400

    Add red circles to map

--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div style={{ color: 'red'}}>{text}</div>;
+import Marker from './Marker.js'
+
  
 class SimpleMap extends Component {
   static defaultProps = {
@@ -21,46 +21,53 @@ class SimpleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
+          <Marker
             lat={41.310726}
             lng={-72.929916}
-            text="NEW HAVEN COUNTY"
+            name="NEW HAVEN COUNTY"
+            color="red"
           />
-          <AnyReactComponent
+          <Marker
             lat={41.763710}
             lng={-72.685097}
-            text="Hartford COUNTY"
+            name="Hartford COUNTY"
+            color="red"
           />
-          <AnyReactComponent
+          <Marker
             lat={41.79}
             lng={-73.24}
-            text="Litchfield COUNTY"
+            name="Litchfield COUNTY"
+            color="red"
           />
-          <AnyReactComponent
+          <Marker
             lat={41.14121}
             lng={-73.26373}
-            text="Fairfield COUNTY"
+            name="Fairfield COUNTY"
+            color="red"
           />
-          <AnyReactComponent
+          <Marker
             lat={41.56232}
             lng={-72.65065}
-            text="Middlesex COUNTY"
+            name="Middlesex COUNTY"
+            color="red"
           />
-          <AnyReactComponent
+          <Marker
             lat={41.35333192}
             lng={-72.092999628}
-            text="New London COUNTY"
+            name="New London COUNTY"
+            color="red"
           />
-          <AnyReactComponent
+          <Marker
             lat={41.87149}
             lng={-72.36869}
-            text="Tolland COUNTY"
-          />
-          <AnyReactComponent
+            name="Tolland COUNTY"
+            color="red"
+            />
+          <Marker
             lat={41.7106543}
             lng={-72.2081338}
-            text="Windham COUNTY"
-          />
+            name="Windham COUNTY"
+            color="red"         />
         </GoogleMapReact>
       </div>
     );
