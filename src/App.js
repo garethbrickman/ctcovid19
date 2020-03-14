9402ac3f28018b392e41efaa7fc87a8977ed70ff
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-03-14 00:25:50 -0400

    Add data to table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Table from './table.js';
 import Chart from './chart.js'
 
 
@@ -17,6 +16,18 @@ class App extends React.Component {
     createTable() {
         const dict = require('./U20200313.json')
 
+        let totalComercialPos = 0
+        let totalComercialNeg = 0
+        let totalStatePos = 0
+        let totalStateNeg = 0
+
+        for (let key in dict) {
+            totalComercialPos += dict[key]['commerciallab']['Positive'];
+            totalComercialNeg += dict[key]['commerciallab']['Negative'];
+            totalStatePos += dict[key]['statelab']['Positive'];
+            totalStateNeg += dict[key]['statelab']['Negative'];
+        }
+
         const table = (
             <div>
                 <table>
@@ -27,33 +38,46 @@ class App extends React.Component {
                         <td rowspan="2"></td>
                         <th colspan="2" scope="colgroup">State Public Health Labs</th>
                         <th colspan="2" scope="colgroup">Commercial or Other Labs</th>
+                        <th colspan="2" scope="colgroup">Combined Totals</th>
                     </tr>
                     <tr>
                         <th scope="col">Positive</th>
                         <th scope="col">Negative</th>
                         <th scope="col">Positive</th>
                         <th scope="col">Negative</th>
+                        <th scope="col">Positive</th>
+                        <th scope="col">Negative</th>
+                        <th scope="col">Total Tested</th>
                     </tr>
                     <tr>
                         <th scope="row">Fairfield</th>
-                        <td>{dict["Fairfield"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["Fairfield"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["Fairfield"]["statelab"]["Positive"]}</td>
                         <td>{dict["Fairfield"]["statelab"]["Negative"]}</td>
+                        <td>{dict["Fairfield"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Fairfield"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Fairfield"]["statelab"]["Positive"] + dict["Fairfield"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Fairfield"]["statelab"]["Negative"] + dict["Fairfield"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Fairfield"]["statelab"]["Negative"] + dict["Fairfield"]["commerciallab"]["Negative"] + dict["Fairfield"]["statelab"]["Positive"] + dict["Fairfield"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">Hartford</th>
-                        <td>{dict["Hartford"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["Hartford"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["Hartford"]["statelab"]["Positive"]}</td>
                         <td>{dict["Hartford"]["statelab"]["Negative"]}</td>
+                        <td>{dict["Hartford"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Hartford"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Hartford"]["statelab"]["Positive"] + dict["Hartford"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Hartford"]["statelab"]["Negative"] + dict["Hartford"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Hartford"]["statelab"]["Negative"] + dict["Hartford"]["commerciallab"]["Negative"] + dict["Hartford"]["statelab"]["Positive"] + dict["Hartford"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">Litchfield</th>
-                        <td>{dict["Litchfield"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["Litchfield"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["Litchfield"]["statelab"]["Positive"]}</td>
                         <td>{dict["Litchfield"]["statelab"]["Negative"]}</td>
+                        <td>{dict["Litchfield"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Litchfield"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Litchfield"]["statelab"]["Positive"] + dict["Litchfield"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Litchfield"]["statelab"]["Negative"] + dict["Litchfield"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Litchfield"]["statelab"]["Negative"] + dict["Litchfield"]["commerciallab"]["Negative"] + dict["Litchfield"]["statelab"]["Positive"] + dict["Litchfield"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">Middlesex</th>
@@ -61,34 +85,59 @@ class App extends React.Component {
                         <td>{dict["Middlesex"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["Middlesex"]["statelab"]["Positive"]}</td>
                         <td>{dict["Middlesex"]["statelab"]["Negative"]}</td>
+                        <td>{dict["Middlesex"]["statelab"]["Positive"] + dict["Middlesex"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Middlesex"]["statelab"]["Negative"] + dict["Middlesex"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Middlesex"]["statelab"]["Negative"] + dict["Middlesex"]["commerciallab"]["Negative"] + dict["Middlesex"]["statelab"]["Positive"] + dict["Middlesex"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">New Haven</th>
-                        <td>{dict["New Haven"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["New Haven"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["New Haven"]["statelab"]["Positive"]}</td>
                         <td>{dict["New Haven"]["statelab"]["Negative"]}</td>
+                        <td>{dict["New Haven"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["New Haven"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["New Haven"]["statelab"]["Positive"] + dict["New Haven"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["New Haven"]["statelab"]["Negative"] + dict["New Haven"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["New Haven"]["statelab"]["Negative"] + dict["New Haven"]["commerciallab"]["Negative"] + dict["New Haven"]["statelab"]["Positive"] + dict["New Haven"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">New London</th>
-                        <td>{dict["New London"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["New London"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["New London"]["statelab"]["Positive"]}</td>
                         <td>{dict["New London"]["statelab"]["Negative"]}</td>
+                        <td>{dict["New London"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["New London"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["New London"]["statelab"]["Positive"] + dict["New London"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["New London"]["statelab"]["Negative"] + dict["New London"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["New London"]["statelab"]["Negative"] + dict["New London"]["commerciallab"]["Negative"] + dict["New London"]["statelab"]["Positive"] + dict["New London"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">Tolland</th>
-                        <td>{dict["Tolland"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["Tolland"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["Tolland"]["statelab"]["Positive"]}</td>
                         <td>{dict["Tolland"]["statelab"]["Negative"]}</td>
+                        <td>{dict["Tolland"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Tolland"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Tolland"]["statelab"]["Positive"] + dict["Tolland"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Tolland"]["statelab"]["Negative"] + dict["Tolland"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Tolland"]["statelab"]["Negative"] + dict["Tolland"]["commerciallab"]["Negative"] + dict["Tolland"]["statelab"]["Positive"] + dict["Tolland"]["commerciallab"]["Positive"]}</td>
                     </tr>
                     <tr>
                         <th scope="row">Windham</th>
-                        <td>{dict["Windham"]["commerciallab"]["Positive"]}</td>
-                        <td>{dict["Windham"]["commerciallab"]["Negative"]}</td>
                         <td>{dict["Windham"]["statelab"]["Positive"]}</td>
                         <td>{dict["Windham"]["statelab"]["Negative"]}</td>
+                        <td>{dict["Windham"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Windham"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Windham"]["statelab"]["Positive"] + dict["Windham"]["commerciallab"]["Positive"]}</td>
+                        <td>{dict["Windham"]["statelab"]["Negative"] + dict["Windham"]["commerciallab"]["Negative"]}</td>
+                        <td>{dict["Windham"]["statelab"]["Negative"] + dict["Windham"]["commerciallab"]["Negative"] + dict["Windham"]["statelab"]["Positive"] + dict["Windham"]["commerciallab"]["Positive"]}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Windham</th>
+                        <td>{totalStatePos}</td>
+                        <td>{totalStateNeg}</td>
+                        <td>{totalComercialPos}</td>
+                        <td>{totalComercialNeg}</td>
+                        <td>{totalStatePos + totalComercialPos}</td>
+                        <td>{totalStateNeg + totalComercialNeg}</td>
+                        <td>{totalStateNeg + totalComercialNeg + totalStatePos + totalComercialPos}</td>
                     </tr>
                 </table>
             </div>
@@ -103,11 +152,11 @@ class App extends React.Component {
                 </header>
                 <h3>Covid 19 in CT as of March 13, 2020</h3>
                 {this.createTable()}
-                <Chart
+                {/* <Chart
                     title={this.state.title}
                     positiveXValues={this.state.positiveXValues}
                     timeYValues={this.state.timeYValues}
-                ></Chart>
+                ></Chart> */}
             </div>
         )
     }
