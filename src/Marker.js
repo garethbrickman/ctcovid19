0d6db301d3b1be6eee49706e62c8f4a7b748c901
Stephen Ranciato <ranciatooo93@Stephens-MacBook-Pro.local>
2020-03-14 16:25:38 -0400

    [ADD] hover confirmed cases on markers

--- src/Marker.js
+++ src/Marker.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './Marker.css';
 
+function useHover() {
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
+
+  const enter = () => setHovered(true)
+  const leave = () => setHovered(false)
+
+  useEffect(() => {
+    ref.current.addEventListener('mouseenter', enter)
+    ref.current.addEventListener('mouseleave', leave)
+    return () => {
+      ref.current.removeEventListener('mouseenter', enter)
+      ref.current.removeEventListener('mouseleave', leave)
+    }
+  }, [ref])
+
+  return [ref, hovered]
+}
+
 const Marker = (props) => {
-    const { color, name} = props;
+    const { color, name, confirmedCases} = props;
+    const [ref, hovered] = useHover()
     return (
       <div className="marker"
         style={{ backgroundColor: color, cursor: 'pointer'}}
         title={name}
-      />
+        ref={ref}
+      >
+        {hovered && <div className="confirmedCases">{confirmedCases} Confirmed Cases</div>}
+      </div>
     );
   };
 
--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker.js'
 
- 
+
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
@@ -21,53 +21,62 @@ class SimpleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          {/* <Marker
+          <Marker
             lat={41.310726}
             lng={-72.929916}
-            name="NEW HAVEN COUNTY"
-            color="red"
+            name="New Haven County"
+            color="gray"
+            confirmedCases="0"
           />
           <Marker
             lat={41.763710}
             lng={-72.685097}
-            name="Hartford COUNTY"
+            name="Hartford County"
             color="red"
-          /> */}
+            confirmedCases="1"
+          />
           <Marker
             lat={41.79}
             lng={-73.24}
-            name="Litchfield COUNTY"
+            name="Litchfield County"
             color="red"
+            confirmedCases="3"
           />
           <Marker
             lat={41.14121}
             lng={-73.26373}
-            name="Fairfield COUNTY"
+            name="Fairfield County"
             color="red"
+            confirmedCases="8"
           />
-          {/* <Marker
+          <Marker
             lat={41.56232}
             lng={-72.65065}
-            name="Middlesex COUNTY"
-            color="red"
+            name="Middlesex County"
+            color="gray"
+            confirmedCases="0"
           />
           <Marker
             lat={41.35333192}
             lng={-72.092999628}
-            name="New London COUNTY"
-            color="red"
+            name="New London County"
+            color="gray"
+            confirmedCases="0"
           />
           <Marker
             lat={41.87149}
             lng={-72.36869}
-            name="Tolland COUNTY"
-            color="red"
+            name="Tolland County"
+            color="gray"
+            confirmedCases="0"
             />
           <Marker
             lat={41.7106543}
             lng={-72.2081338}
-            name="Windham COUNTY"
-            color="red"         /> */}
+            name="Windham County"
+            color="gray"
+            confirmedCases="0"         
+            />
         </GoogleMapReact>
       </div>
     );
