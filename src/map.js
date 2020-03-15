c010420226a822ea7eefe9185d7f20b9b736f20d
Stephen Ranciato <ranciatooo93@Stephens-MacBook-Pro.local>
2020-03-14 21:14:34 -0400

    [ADD] total count of cases

--- src/map.js
+++ src/map.js
@@ -14,10 +14,15 @@ class SimpleMap extends Component {
  
   render() {
     const dict = require('./m20200314.json')
+    var total = 0
+    for(let value of Object.values(dict)){
+      total += value;
+    }
+    // const total = dict => Object.values(dict).reduce((a, b) => a + b);
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
-      <h1>Counties with Positive Cases as of March 14th, 2020</h1>
+      <h1>Counties with Positive Cases as of March 14th, 2020 | Total cases = {total}</h1>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAFl0p7Zj1aSMscGUdeWlGkrhqZrFyogZg' }}
           defaultCenter={this.props.center}
@@ -87,14 +92,14 @@ class SimpleMap extends Component {
             confirmedCases={dict['Windham']}        
             />
         </GoogleMapReact>
-        <div>
-          <p>
-            source: <a class="link" href="https://portal.ct.gov/coronavirus">https://portal.ct.gov/coronavirus</a><br/>
-            source for New Haven: <a class="link" href="https://www.wtnh.com/news/connecticut/new-haven/yale-says-member-of-university-tests-positive-for-covid-19/">https://www.wtnh.com/news/connecticut/new-haven/yale-says-member-of-university-tests-positive-for-covid-19/</a><br/>
-            <br/>
-          </p>
-          <p></p>
-        </div>
+      <div>
+        <p>
+          source: <a class="link" href="https://portal.ct.gov/coronavirus">https://portal.ct.gov/coronavirus</a><br/>
+          source for New Haven: <a class="link" href="https://www.wtnh.com/news/connecticut/new-haven/yale-says-member-of-university-tests-positive-for-covid-19/">https://www.wtnh.com/news/connecticut/new-haven/yale-says-member-of-university-tests-positive-for-covid-19/</a><br/>
+          <br/>
+        </p>
+        <p></p>
+      </div>
       </div>
     );
   }
